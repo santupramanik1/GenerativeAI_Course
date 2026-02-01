6b30227c7252d81a5e36e0ade884a917e802baa3
Santu pramanik <santu700141@gmail.com>
2026-02-01 23:58:54 +0530

    Add apikey in to the env file

--- LLM.js
+++ LLM.js
@@ -1,7 +1,9 @@
 import {GoogleGenAI} from "@google/genai";
 import readlineSync from "readline-sync";
+import dotenv from "dotenv"
 
-const ai = new GoogleGenAI({apiKey: "AIzaSyChtBlDuVm_jVGwZuXfL6JhWFso3JsBJVQ"});
+dotenv.config()
+const ai = new GoogleGenAI({apiKey: process.env.API_KEY});
 
 const history = [];
 async function chatting(userProblem) {
